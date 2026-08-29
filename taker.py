1dbe2b97f1cd7da8bb905f4144de385f291431a1
yuexiafeng33 <2574975630@qq.com>
2026-08-29 14:13:56 +0800

    ros发布者的完整版

--- taker.py
+++ taker.py
@@ -1,2 +1,17 @@
-#!#!/usr/bin/env python3
+#!/usr/bin/env python3
+
 import rospy
+from std_msgs.msg import String
+
+if __name__ == "__main__":
+    rospy.init_node("cc")
+
+    pub = rospy.Publisher("hello", String, queue_size=10)
+
+    msg = String()
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        msg.data = "cc"
+        pub.publish(msg)
+        rate.sleep()
