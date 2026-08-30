2ec0281ecb0541d6b81c60a5b60ef784e53af08e
yuexiafeng33 <2574975630@qq.com>
2026-08-30 13:09:35 +0800

    完整的ros通信最小代码

---
+++ work_space/taker.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String
+
+if __name__ == "__main__":
+    rospy.init_node("cc")
+
+    pub = rospy.Publisher("hello", String, queue_size=10)
+
+    msg = String()
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        msg.data = "cc"
+        pub.publish(msg)
+        rospy.loginfo("我发布的数据：%s",msg.data)
+        rate.sleep()
